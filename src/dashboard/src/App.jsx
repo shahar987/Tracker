eb70417056f81d93b56c3b8c3385d7aba7ea1928
Amit <amitkastel3@gmail.com>
2022-03-09 10:43:13 +0200

    added react router

--- src/dashboard/src/App.jsx
+++ src/dashboard/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import ClientsPage from './pages/ClientsPage/ClientsPage';
+import ClientPage from './pages/ClientPage/ClientPage';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+        <Route path='/' element={ <HomePage /> } />
+        <Route path='/login' element={ <LoginPage/> } />
+        <Route path='/clients' element={ <ClientsPage/> } />
+        <Route path='/client' element={ <ClientPage/> } />
+    </Routes>
   )
 }
 
