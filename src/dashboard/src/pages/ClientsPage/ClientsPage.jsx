637cd1fa6dac2c84eb62e599ccec8d6d241390b5
shellyazulay <shellyazulay644@gmail.com>
2022-04-17 23:18:37 +0300

    Clients Page

--- src/dashboard/src/pages/ClientsPage/ClientsPage.jsx
+++ src/dashboard/src/pages/ClientsPage/ClientsPage.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
+import Data from "../../components/ClientList/Data";
+import Card from "../../components/ClientList/Card";
+import Buttons from "../../components/ClientList/Buttons";
+import Typography from "@mui/material/Typography";
 
 const ClientsPage = () => {
-  return (
-    <div>ClientsPage</div>
-  )
-}
+  const [item, setItem] = useState(Data);
 
-export default ClientsPage
+  const filterItem = (curcat) => {
+    const newItem = Data.filter((newVal) => {
+      return newVal.category === curcat;
+    });
+    setItem(newItem);
+  };
+
+  return (
+    <>
+      <div>
+        <Typography
+          sx={{ fontFamily: "Roboto", marginTop: 2, marginBottom: 2 }}
+          variant="h2"
+          align="center"
+        >
+          TRACKR TEST RESULTS
+        </Typography>
+        <Buttons filterItem={filterItem} setItem={setItem} />
+        <Card item={item} />
+      </div>
+    </>
+  );
+};
+
+export default ClientsPage;
